f699339869273ed3c26eba00f19fd1bcdb0e3dc1
Mateusz Grzeliński <grzelinskimat@gmail.com>
2023-07-09 22:32:18 +0200

    Update

--- setup_auto_update.py
+++ setup_auto_update.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 import configparser
-import re
+import logging
+from typing import Iterable
 
 import requests
 import semver
@@ -9,13 +10,28 @@ from requests_html import HTMLSession
 
 import setup
 
-if __name__ == '__main__':
+ACTIONLINT_RELEASES = 'https://github.com/rhysd/actionlint/releases/'
+
+SETUP_CFG = './setup.cfg'
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
+
+
+def get_release_page_links():
     session = HTMLSession()
+    r = session.get(ACTIONLINT_RELEASES)
+    return r.html.links
 
-    r = session.get('https://github.com/rhysd/actionlint/releases/')
 
-    links = r.html.links
+def get_checksum_file(newest_release_link: str) -> str:
+    checksums_file = requests.get('https://github.com' + newest_release_link)
+    # checksum_file_name = newest_release_link.split('/')[-1]
+    content = checksums_file.content.decode('utf-8')
+    return content
 
+
+def get_newest_release_link(links: Iterable[str]) -> str:
     newest_release_link = None
     for link in links:
         link: str
@@ -23,30 +39,40 @@ if __name__ == '__main__':
         if link.endswith('_checksums.txt'):
             newest_release_link = link
             break
-    print(newest_release_link)
-    # '/rhysd/actionlint/releases/download/v1.6.24/actionlint_1.6.24_checksums.txt'
-    newest_version_str = newest_release_link.split('/')[-2].lstrip('v')
+    return newest_release_link
 
-    newest_version = semver.Version.parse(newest_version_str)
-    current_version = semver.Version.parse(setup.ACTIONLINT_VERSION)
-    if newest_version.compare(current_version) != 1:
-        exit(0)
 
-    checksums_file = requests.get('https://github.com' + newest_release_link)
-    content = checksums_file.content.decode('utf-8')
-
+def update_config(checksum_file_content: str, newest_version_str: str) -> None:
+    """Update tag and checksum in config file"""
     config = configparser.ConfigParser()
-    config.read('./setup.cfg')
+    config.read(SETUP_CFG)
     files_to_download = config['files-to-download']
     files_to_download['tag'] = newest_version_str
-    for line in content.split('\n'):
+    for line in checksum_file_content.split('\n'):
         if not line:
             continue
         checksum, file = line.split()
         file_ending = file.split('_')[-2:]
         file_ending = '_' + '_'.join(file_ending)
         files_to_download[file_ending] = checksum
-    with open('./setup.cfg', 'w') as file:
+    with open(SETUP_CFG, 'w') as file:
         config.write(file, space_around_delimiters=True)
 
-    checksum_file_name = newest_release_link.split('/')[-1]
+
+def main():
+    links = get_release_page_links()
+    newest_release_link = get_newest_release_link(links)
+    newest_version_str = newest_release_link.split('/')[-2].lstrip('v')
+    log.info(f'Newest version: {newest_version_str}')
+    newest_version = semver.Version.parse(newest_version_str)
+    current_version = semver.Version.parse(setup.ACTIONLINT_VERSION)
+    if newest_version.compare(current_version) != 1:
+        log.info('Local version is newest, all good. Exiting.')
+        exit(0)
+    checksum_file_content = get_checksum_file(newest_release_link)
+    update_config(checksum_file_content, newest_version_str)
+    log.info('Local config updated.')
+
+
+if __name__ == '__main__':
+    main()
